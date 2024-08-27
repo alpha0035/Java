9986f2a6061cac1940a52f4b20c192d5130e12be
Alpha <alpha07eg@gmail.com>
2024-08-27 16:48:28 +0700

    using static fields and methods

--- working_with_static/staticFields.java
+++ working_with_static/staticFields.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class staticFields {
     private static Scanner sc= new Scanner(System.in);
-    static private int x=0;
+    static private int x=1;
     // same effect
     public static void main(String[] args) {
         int y=0;
+        staticFields obj= new staticFields();
         for (int i = 0; i < 5; i++) {
-            print();
+            obj.print();
         }
     }
-    private static void print(){
+    private void print(){
         System.out.printf("%d", x);
         x+=1;
     }
