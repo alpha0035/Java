d7d283fe33d1aa82c2d79b9f3a5e309323afd5b1
Alpha <alpha07eg@gmail.com>
2024-09-03 13:20:02 +0700

    using an executor

---
+++ programming_threads/usingExecutor.java
@@ -0,0 +1,38 @@
+package programming_threads;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class usingExecutor {
+    public static void main(String[] args) {
+        ScheduledThreadPoolExecutor pool= new ScheduledThreadPoolExecutor(25);
+        // Runnable alp, bta, gma;
+        // alp= new newLaunchEvent("Alpha");
+        // bta= new newLaunchEvent("Beta");
+        // gma= new newLaunchEvent("Gamma");
+        for (int i = 20; i >= 0; i--) {
+            pool.schedule(new newCountDownClock(i), 20-i, TimeUnit.SECONDS);
+        }
+        pool.schedule(new newLaunchEvent("Alpha"), 5, TimeUnit.SECONDS);
+        pool.schedule(new newLaunchEvent("Beta"), 10, TimeUnit.SECONDS);
+        pool.schedule(new newLaunchEvent("Gamma"), 15, TimeUnit.SECONDS);
+        pool.shutdown();
+    }
+}
+class newCountDownClock implements Runnable{
+    private int t;
+    public newCountDownClock(int t){
+        this.t=t;
+    }
+    public void run(){
+        System.out.println("T minus "+t);
+    }
+
+}
+class newLaunchEvent implements Runnable{
+    private String mess;
+    public newLaunchEvent(String s){mess=s;}
+    public void run(){
+        System.out.println(mess);
+    }
+}
