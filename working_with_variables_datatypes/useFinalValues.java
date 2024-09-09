5a60a3920618cf21eb284b94980aafafade32728
Alpha <alpha07eg@gmail.com>
2024-09-09 19:43:53 +0700

    using final values (constants)

---
+++ working_with_variables_datatypes/useFinalValues.java
@@ -0,0 +1,10 @@
+package working_with_variables_datatypes;
+
+public class useFinalValues {
+    public static void main(String[] args) {
+        final int x= 10;
+        // constant value in java can be create by using final keyword
+        // constant can't be changed
+        System.out.println("This number is constant value: "+x);
+    }
+}
