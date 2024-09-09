3d4e8d25a693983e9841ed256a566a93b40860e7
Alpha <alpha07eg@gmail.com>
2024-09-09 19:52:48 +0700

    using primitive data types

---
+++ working_with_variables_datatypes/primitiveTypes.java
@@ -0,0 +1,14 @@
+package working_with_variables_datatypes;
+// primitive data types is defined by the language itself
+
+public class primitiveTypes {
+    // in Java, primitive types include:
+    byte x;     // 8-bit integer
+    short y;    //16-bit integer
+    int z;      //32-bit integer
+    long t;     //64-bit integer
+    float u;    //32-bit floating-point 
+    double v;   //64-bit floating-point
+    char a;     //16-bit character Unicode scheme
+    boolean f;  //true or false
+}
